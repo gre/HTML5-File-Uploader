81ef7d895d4e44584545cdab3d2b497bc4ffc28e
Renaudeau Gaetan <gre@zenexity.fr>
2010-04-30 17:45:06 +0200

    asynchronous upload support with play framework

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,70 @@
 package controllers;
 
+import java.io.File;
+
 import play.mvc.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.annotation.processing.FilerException;
+import javax.imageio.ImageIO;
+import play.Logger;
+import play.Play;
+import play.libs.Files;
+import play.libs.IO;
+import play.libs.Images;
+
 
 public class Application extends Controller {
+    
+    
+    static boolean fileExists(File file) {
+        return (Play.getFile("/public/files/"+file.getName()).exists());
+    }
+    static File putFile(File file)  {
+        if (file != null) {
+            try {
+                File f = Play.getFile("/public/files/" + file.getName());
+                File dir = new File(f.getParent());
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+                f.createNewFile();
 
+                Files.copy(file, f);
+
+                return f;
+            } catch (IOException e) {
+                return null;
+            }
+        }
+     return null;
+ }
+    
+    
     public static void index() {
         render();
+    }
+    
+    public static void postFile(File file) {
+        String status = "";
+        if (file == null) {
+            status = "fileRequired";
+            render(status);
+        }
+        if (fileExists(file)) {
+            status = "fileExists";
+            render(status);
+        }
+        File f = putFile(file);
+        if (f == null) {
+            status = "internalError";
+            render(status);
+        }
+        Logger.info("postedFile : %s", f.getPath());
+        status = "sent";
+        render(status);
     }
 
 }
--- public/javascripts/postFile.js
+++ public/javascripts/postFile.js
@@ -5,7 +5,20 @@
     return {
       
       init: function() {
-        
+        $('.postFile form input').val('').change(function(){
+          var postFileNode = $(this).parents('.postFile');
+          $('form', postFileNode).ajaxSubmit({
+            beforeSubmit: function() {
+              $('.message', postFileNode).empty();
+              $('img.postFileLoad', postFileNode).show();
+            },
+            success: function(responseText, statusText, xhr) {
+              $('input', xhr).val('');
+              $('img.postFileLoad', postFileNode).hide();
+              $('.message', postFileNode).append(responseText);
+            }
+          });
+        });
         
       }
       
