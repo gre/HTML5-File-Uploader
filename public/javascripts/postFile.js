2b91e2638873fa9be112e1eae7fa9856429c6f49
Renaudeau Gaetan <gre@zenexity.fr>
2010-04-30 17:56:05 +0200

    begin a drop file area (not work yet)

--- public/javascripts/postFile.js
+++ public/javascripts/postFile.js
@@ -20,6 +20,23 @@
           });
         });
         
+        $('.postFile form .dropzone').each(function(){
+            $(this).droppable($(this).attr('accept'),
+                // Drag enter
+                function(e) {
+                    $(this).addClass('filehover');
+                },
+                // Drag leave
+                function() {
+                    $(this).removeClass('filehover');
+                },
+                // Drop!
+                function(e) {
+                  debugger
+                    //$('ul', this).append('<li>' + e.dataTransfer.files[0].fileName + '</li>')
+                });
+          });
+                
       }
       
     }
